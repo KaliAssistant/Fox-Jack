bd80130053a09a1a9ba653c12d54b46840e51fde
kaliassistant <1325077@gmail.com>
2025-07-24 12:50:37 +0800

    typo

--- src/2p_modswitch/etc/init.d/modswitchd/MOD0.sh
+++ src/2p_modswitch/etc/init.d/modswitchd/MOD0.sh
@@ -4,7 +4,7 @@ NAME="DEBUG"
 
 init() {
     /etc/init.d/M21networkinit start
-    /etc/init.d/M49ntpd start
+    /etc/init.d/M49ntp start
     /etc/init.d/M50sshd start
     /etc/init.d/M50usbgtgadgets start /etc/gt/debug.scheme
     /etc/init.d/M51usbnet start
